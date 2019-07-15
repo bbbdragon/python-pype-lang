56964ce588cafd337766fa0c388e9e97fc3a75b8
Bennett Bullock <bennettbullock36@gmail.com>
2019-07-15 18:56:40 -0300

    Long description variables commented out because of errors

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    LONG_DESCRIPTION=fh.read()
+#with open("README.md", "r") as fh:
+#    LONG_DESCRIPTION=fh.read()
 
 setup(name='python-pype-lang',
       version='1.0',
       description='Python-integrated functional programming language - Provides easy-to-use pseudo-macros for common functional programming tasks such as maps, reduces, filters, conditionals, dictionary manipulations, and list manipulations..',
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type="text/markdown",
+      #long_description_content_type="text/markdown",
+      #long_description=LONG_DESCRIPTION,
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
