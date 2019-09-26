1722cc714dc2243cb48038cb1353da158329350b
Bennett Bullock <bennettbullock36@gmail.com>
2019-09-26 13:22:25 -0300

    Adding a lot of new functionality

--- pype/time_helpers.py
+++ pype/time_helpers.py
@@ -1,6 +1,7 @@
 import datetime as dt
 from copy import deepcopy
 from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
 
 BEGIN=dt.datetime(year=1970,month=1,day=1)
 END=dt.datetime(year=2030,month=1,day=1)
@@ -11,7 +12,7 @@ def date_range(begin,end):
     d=deepcopy(begin)
     rng=[d]
 
-    while d <= end:
+    while d < end:
 
         d+=DAY
 
@@ -19,14 +20,76 @@ def date_range(begin,end):
 
     return rng
 
+
+def add_days(date,days=1):
+
+    return date+dt.timedelta(days=int(days))
+
+
+def add_months(date,months=1):
+
+    return date+relativedelta(months=int(months))
+
+
+def date_range_days(begin,days):
+
+    return [begin+dt.timedelta(days=d) for d in range(days)]
+
+
+def date_string_range(beginDateString,endDateString):
+
+    begin=parse(beginDateString)
+    end=parse(endDateString)
+    rng=date_range(begin,end)
+    dateStrings=[date_string(d) for d in rng]
+
+    return dateStrings
+
+    
 def date_int(date):
 
     return int(f'{date.year}'+'{0:0=2d}'.format(date.month)+'{0:0=2d}'.format(date.day))
 
 
 def date_string(date):
-
+    
     return date.strftime("%Y-%m-%d")
+
+
+def begin_next_week(date):
+
+    weekday=date.weekday()
+    offset=7-weekday
+    newDate=date+dt.timedelta(days=offset)
+
+    return newDate
+
+
+def begin_this_week(date):
+
+    weekday=date.weekday()
+    newDate=date-dt.timedelta(days=weekday)
+
+    return newDate
+
+
+def begin_this_week_string(dateString):
+
+    return date_string(begin_this_week(parse(dateString)))
+
+
+def begin_next_week_string(dateString):
+
+    return date_string(begin_next_week(parse(dateString)))
+
+
+def today():
+
+    return dt.datetime.now()
+
+def today_string():
+
+    return date_string(today())
 
 
 def date_to_month(date):
@@ -74,6 +137,28 @@ def date_string_to_weekday_int(dateString):
 
     return from_cache(dateString,DATE_TO_WEEKDAY_CACHE)
 
+
+def date_string_to_month_day_int(dateString):
+
+    return int(dateString[-2:])
+
+
+def date_string_to_bi_weekday_int(dateString):
+
+    dayInt=date_string_to_month_day_int(dateString)
+
+    if dayInt < 14:
+
+        return dayInt
+
+    return dayInt-14
+
+
+def date_string_to_weekend_weekday_int(dateString):
+
+    wd=date_string_to_weekday_int(dateString)
+
+    return 0 if wd < 5 and wd >= 0 else 1
 
 
 MONTH_TO_INT_CACHE={}
@@ -124,6 +209,23 @@ def increment_weekday(weekdayInt):
     return (weekdayInt + 1) % 7
 
 
+def increment_date(date,days=1):
+
+    return date+dt.timedelta(days=int(days))
+
 def increment_date_string(dateString,days=1):
 
     return date_string(parse(dateString)+dt.timedelta(days=int(days)))
+
+
+def tomorrow():
+
+    return increment_date_string(today())
+
+
+def weekday_interval_date_strings(weekBeginDateString,interval):
+
+    beginDate=increment_date_string(weekBeginDateString,interval[0])
+    endDate=increment_date_string(weekBeginDateString,interval[1])
+
+    return date_string_range(beginDate,endDate)
