fe938f3e513500e17c77551c1a70b21eb4fd21d9
Bennett Bullock <bennettbullock36@gmail.com>
2019-10-02 19:28:35 -0300

    More wild and wonderful mappings

--- pype/time_helpers.py
+++ pype/time_helpers.py
@@ -36,6 +36,21 @@ def date_range_days(begin,days):
     return [begin+dt.timedelta(days=d) for d in range(days)]
 
 
+def date_int_range_days(begin,days):
+
+    return [date_int(d) for d in date_range_days(begin,days)]
+
+
+def date_week_range(begin):
+
+    return date_range_days(begin,7)
+
+
+def date_string_week_range(begin):
+
+    return [date_to_date_string(d) for d in date_range_days(begin,7)]
+
+
 def date_string_range(beginDateString,endDateString):
 
     begin=parse(beginDateString)
@@ -97,25 +112,60 @@ def date_to_month(date):
     return dt.datetime(year=date.year,month=date.month,day=1)
 
 DATE_RANGE=date_range(BEGIN,END)
+DATE_STRING_RANGE=[]
 DATE_TO_INT_CACHE={}
 INT_TO_DATE_CACHE={}
 DATE_TO_STRING_CACHE={}
 DATE_TO_WEEKDAY_CACHE={}
+DATE_STRING_TO_DATE_CACHE={}
+DATE_STRING_TO_NEXT_DATE_STRING_CACHE={}
 
 for date in DATE_RANGE:
 
     dateString=date_string(date)
     dateInt=date_int(date)
 
+    DATE_STRING_RANGE.append(dateString)
+
     DATE_TO_INT_CACHE[dateString]=dateInt
     DATE_TO_WEEKDAY_CACHE[dateString]=date.weekday()
     INT_TO_DATE_CACHE[dateInt]=dateString
     DATE_TO_STRING_CACHE[date]=dateString
+    DATE_STRING_TO_DATE_CACHE[dateString]=date
+
+
+for (date1,date2) in zip(DATE_STRING_RANGE,DATE_STRING_RANGE[1:]):
+
+    DATE_STRING_TO_NEXT_DATE_STRING_CACHE[date1]=date2
+
+
+from functools import reduce
+
+def append_last_from_cache(ls,cache):
+
+    el=ls[-1]
+    newEl=cache[el]
+
+    ls.append(newEl)
+
+    return ls
+
+def date_string_range_days(beginDateString,numDays):
+
+    return reduce(lambda ls,x: append_last_from_cache(ls,
+                                                      DATE_STRING_TO_NEXT_DATE_STRING_CACHE),
+                  range(numDays),
+                  [beginDateString])
 
 
 def from_cache(x,cache):
 
     return cache[x]
+
+
+def date_string_to_date(dateString):
+
+    return from_cache(dateString,DATE_STRING_TO_DATE_CACHE)
 
 
 def date_string_to_int(dateString):
@@ -197,6 +247,10 @@ WEEKDAYS=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'
 def weekday_int_to_string(i):
 
     return WEEKDAYS[i]
+
+def date_string_to_weekday_string(dateString):
+
+    return weekday_int_to_string(date_string_to_weekday_int(dateString))
 
 
 def today_date_string():
