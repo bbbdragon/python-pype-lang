6a618b82a22055b4b358bc3965521b65acbf6b74
Bennett Bullock <bennettbullock36@gmail.com>
2019-10-16 14:55:04 -0300

    Updated to dot notation and applied optimize to all functions

--- examples/cyk.py
+++ examples/cyk.py
@@ -17,19 +17,21 @@ same table structure is being updated with each iteration - the code is function
 the underlying implementation does not guarantee immutability.
 '''
 from pype import pype as p
-from pype import _,_0,_1,_2,_l
+from pype import _,_0,_1,_2,_l as tup
 import pprint as pp
 from pype.vals import PypeVal as v
 from pype import _concat as _c
-from pype import _assoc as _a
-from pype import _dissoc as _d
+from pype import _assoc as a
+from pype import _dissoc as d
+from pype import _d as db
 from pype import _merge as _m
-from pype import _p 
+from pype import _p as ep
 from pype import Quote as q
+from pype.optimize import optimize
 from itertools import product
 from pype.vals import lenf
 from pype.helpers import *
-
+from pype import _iff,_if,_iffp
 '''
 In the grammar string, we define a context-free grammar.  The first symbol in each
 line is the left-hand-side symbol, or lhs.  The second symbol is the first 
@@ -42,6 +44,7 @@ VP V NP
 S NP VP
 '''
 
+@optimize
 def read_grammar(grammarString):
     '''
     We read the grammar string and convert it into a JSON of the form 
@@ -81,7 +84,7 @@ def read_grammar(grammarString):
     from this list, so now our JSON is:{rhs1:{rhs2:{'rhs1':rhs1,'rhs2':rhs2,
     'lhs':lhs},...},...}
 
-    [[_['lhs']]],
+    [[_.lhs]],
 
     Now, we want to extract only the 'lhs' from the JSON keyed by rhs1 and rhs2, 
     producing a JSON of the form: {rhs1:{rhs2:lhs,...},...}.
@@ -100,22 +103,11 @@ def read_grammar(grammarString):
               (merge_ls_dct,_,'rhs1'),
               [(merge_ls_dct,_,'rhs2')],
               [[_0]],
-              [[_['lhs']]],
+              [[_.lhs]],
             )
 
 
-def range_for_span(span,fArg):
-    '''
-    CYK requires three sequences of numbers, the span, the begin index, and the 
-    partition.  This takes a span, which is 1 to the final index of the sequence,
-    and it takes a quoted fArg.  Because [(range_list,0,fArg)], produces a list of
-    lists, we use the flatten_list helper to convert it into a single list.
-    '''
-    return p( span,
-              [(range_list,0,fArg)],
-              flatten_list)
-
-
+@optimize
 def partitions(seq):
     '''
     In our strategy, we do not want to iterate over a static table, but rather
@@ -127,65 +119,92 @@ def partitions(seq):
      'end2':end2}
 
     It defines a partition of the string which the parser will evaluate to generate
-    and insert new symbols.  To do this, we first evaluate the following:
+    and insert new symbols.  To do this, we build a dict with the span, or length
+    of the partition:
 
-    (range_list,1,_),
-    
-    This is a list of integers from 1 to the length of the sequence.
+              db('spans',(range_list,1,_)), # span
 
-    (product,_,
-             (range_for_span,_,q(v(stLen)-_)),
-             (range_for_span,_,q(_))),
+    Then, we build a list of i1Begin indices:
 
-    Now, we take the Cartesian product of 3 sequences.  The first is the span range
-    evaluated above, and marked by _.
+              a('iBegin1s',ep(_.spans,
+                              [(range_list,0,stLen-_)],
+                              flatten_list)),
 
-    (range_for_span,_,q(v(stLen)-_)),
+    And finally, a list of partitions, which will define where the first part of the 
+    partition begins and the second part ends:
 
-    This second is the range of begin1 indices.  Notice we are passing the range of
-    the span, but we are also passing an q(stLen-_).  stLen-_ evaluates to a LamTup.
-    TODO: explain this!.  q() means that we are quoting the expression, and do not
-    want the pype interpreter to evaluate this expression in this function, but rather
-    to pass it into range_for_span as an fArg, to be evaluated there.
-    
-    (range_for_span,_,q(_))),
+              a('partitions',ep(_.spans,
+                                [(range_list,0,_)],
+                                flatten_list)),
 
-    This third is the range of the partition indices.  Again, we use the quote.
+    Let's take the cartesian product of these three lists, giving us a tuple of 
+    (span, iBegin1, partition):
 
-    (zip_to_dicts,_,'span','begin1','partition'),
+              (cartesian,_.spans,_.iBegin1s,_.partitions),
 
-    zip_to_dicts is a helper which iterates through a sequence of tuples and builds
-    a dictionary with the consecutively ordered keys.
+    Cast the tuple to a dictionary:
 
-    [_a('end1',_['begin1']+_['partition'])],
+              (zip_to_dicts,_,'span','i1Begin','partition'),
 
-    Now that we have a list of dictionaries, we add a field, 'end1', and assign the
-    result of the following expression.
+    And now we fill in i1End, i2Begin, and i2End.
 
-    [_a('begin2',_['end1']+1)],
-    [_a('end2',_['begin1']+_['span'])],
+              [a('i1End',_.i1Begin + _.partition)],
+              [a('i2Begin',_.i1End + 1)],
+              [a('i2End',_.i1Begin + _.span)],
 
-    We add 'begin2' and 'end2' in the same way.
+    Delete the 'partition' field, but keep the 'span' field:
 
-    [_d('partition','span')],
+              [d('partition')],
 
-    Since we no longer need these fields, we remove them from the dictionaries.
+    Get rid of any combinations where i2End is less than i2Begin, and remove 
+    repeating partition JSON's:
+
+              {_.i2Begin <= _.i2End},
+              unique_dcts,
+
+    Finally, sort this list of JSON's.  First, we sort it by 'span', so that the 
+    shortest JSON's appear first.  Then, sort it by i1Begin.  This means that the
+    first partitions in the list will be over two symbols:
+
+              (sort_by_keys,_,'span','i1Begin'),
+
+    The end result looks like this:
+
+    [{'i1Begin': 0, 'i1End': 0, 'i2Begin': 1, 'i2End': 1, 'span': 1},
+    {'i1Begin': 1, 'i1End': 1, 'i2Begin': 2, 'i2End': 2, 'span': 1},
+    {'i1Begin': 2, 'i1End': 2, 'i2Begin': 3, 'i2End': 3, 'span': 1},
+    {'i1Begin': 3, 'i1End': 3, 'i2Begin': 4, 'i2End': 4, 'span': 1},
+    {'i1Begin': 0, 'i1End': 0, 'i2Begin': 1, 'i2End': 2, 'span': 2},
+    {'i1Begin': 0, 'i1End': 1, 'i2Begin': 2, 'i2End': 2, 'span': 2},
+    ...
+    {'i1Begin': 3, 'i1End': 3, 'i2Begin': 4, 'i2End': 7, 'span': 4},
+    {'i1Begin': 3, 'i1End': 4, 'i2Begin': 5, 'i2End': 7, 'span': 4},
+    {'i1Begin': 3, 'i1End': 5, 'i2Begin': 6, 'i2End': 7, 'span': 4},
+    {'i1Begin': 3, 'i1End': 6, 'i2Begin': 7, 'i2End': 7, 'span': 4}]
     '''
     stLen=len(seq)
 
     return p( stLen,
-              (range_list,1,_),
-              (product,_,
-                       (range_for_span,_,q(stLen-_)),
-                       (range_for_span,_,q(_))),
-              (zip_to_dicts,_,'span','begin1','partition'),
-              [_a('end1',_['begin1']+_['partition'])],
-              [_a('begin2',_['end1']+1)],
-              [_a('end2',_['begin1']+_['span'])],
-              [_d('partition','span')],
+              db('spans',(range_list,1,_)), # span
+              a('iBegin1s',ep(_.spans,
+                              [(range_list,0,stLen-_)],
+                              flatten_list)),
+              a('partitions',ep(_.spans,
+                                [(range_list,0,_)],
+                                flatten_list)),
+              (cartesian,_.spans,_.iBegin1s,_.partitions),
+              (zip_to_dicts,_,'span','i1Begin','partition'),
+              [a('i1End',_.i1Begin + _.partition)],
+              [a('i2Begin',_.i1End + 1)],
+              [a('i2End',_.i1Begin + _.span)],
+              [d('partition')],
+              {_.i2Begin <= _.i2End},
+              unique_dcts,
+              (sort_by_keys,_,'span','i1Begin'),
             )
 
 
+@optimize
 def init_table(seq):
     '''
     This takes a sequence and initializes a table.  The table is a JSON of form
@@ -198,18 +217,18 @@ def init_table(seq):
     symbol in the sequence, 0 for the first, 1 for the second, etc. tree1 is a tree
     representation of that symbol, which is a list of form [seq1].
 
-    (zip,(range,len),_),
+              (zip,(range,len),_),
 
     Create a list of tuples containing the index and the symbol.
 
-    [{_0:{_0:{'lhs':_1,
-              'tree':_l(_1)}}}],
+              [{_0:{_0:{'lhs':_1,
+                        'tree':tup(_1)}}}],
 
     _0 refers to the index, so we are creating a list of JSON's of form 
 
     {index1:{index1:{'lhs':seq1,'tree':tree1}}
 
-    [(dct_merge_vals,),{}],
+              [(dct_merge_vals,),{},_],
 
     dct_merge_vals merges two dictionaries that have embedded dictionaries as values.
     We are doing this as a reduce, with an empty dictionary, {}, as the starting value.
@@ -217,19 +236,20 @@ def init_table(seq):
     return p( seq,
               (zip,(range,len),_),
               [{_0:{_0:{'lhs':_1,
-                        'tree':_l(_1)}}}],
-              [(dct_merge_vals,),{}],
+                        'tree':tup(_1)}}}],
+              [(dct_merge_vals,),{},_],
             )
 
 
+@optimize
 def get_lhs(begin1,begin2,end1,end2,table,grammar):
     '''
     This takes a table, the variables of a partition, and a grammar, and returns a 
     JSON of form {'lhs':lhs1,'tree':tree1} if there is an element in the table
     from which the grammar can derive an lhs.  
 
-    {'rhs1':_[begin1,end1],
-     'rhs2':_[begin2,end2]},
+              {'rhs1':_[begin1,end1],
+               'rhs2':_[begin2,end2]},
 
     If the sequence of keys is present in the dictionary, we return the element keyed
     by those keys.  Otherwise, we return False.
@@ -242,9 +262,9 @@ def get_lhs(begin1,begin2,end1,end2,table,grammar):
     because we are going to reference them in the next two lines - this how we can
     use dict builds for scoping.
 
-    _a('lhs',(get_or_false,grammar,
-                           _['rhs1','lhs'],
-                           _['rhs2','lhs'])),
+              a('lhs',(get_or_false,grammar,
+                                     _.rhs1.lhs,
+                                     _.rhs2.lhs)),
 
     get_or_false has the same functionality as the indexing.  However, we use it 
     because the alternative expression:
@@ -257,11 +277,12 @@ def get_lhs(begin1,begin2,end1,end2,table,grammar):
     first rhs, and the lhs of rhs2 is the second lhs.  We assign the result to 'lhs'
     in the main JSON.
 
-     {_['lhs']:_p( _a('tree',_l(_['lhs'],
-                                _['rhs1','tree'],
-                                _['rhs1','tree']),
-                            _d('rhs1','rhs2')),
-     'else':False}
+              _iff(_.lhs,ep(a('tree',tup(_.lhs,
+                                          _.rhs1.tree,
+                                          _.rhs2.tree)),
+                            d('rhs1','rhs2'))),
+
+    _iff returns a switch dict: {_:fArg,'else':False}.
 
     Remember, in a switch dict, if the key evaluates as True, then the corresponding
     expression is evaluated and returned.  If the grammar cannot find an lhs in the
@@ -274,21 +295,62 @@ def get_lhs(begin1,begin2,end1,end2,table,grammar):
 
     Finally, we get rid of 'rhs1','rhs2', which we don't need and return the JSON.
     '''
-
     return p( table,
               {'rhs1':_[begin1,end1],
                'rhs2':_[begin2,end2]},
-              _a('lhs',(get_or_false,grammar,
-                                     _['rhs1','lhs'],
-                                     _['rhs2','lhs'])),
-              {_['lhs']:_p( _a('tree',_l(_['lhs'],
-                                         _['rhs1','tree'],
-                                         _['rhs2','tree'])),
-                            _d('rhs1','rhs2')),
-               'else':False}
+              a('lhs',(get_or_false,grammar,
+                                     _.rhs1.lhs,
+                                     _.rhs2.lhs)),
+              _iff(_.lhs,ep(a('tree',tup(_.lhs,
+                                          _.rhs1.tree,
+                                          _.rhs2.tree)),
+                            d('rhs1','rhs2'))),
             )
 
 
+@optimize
+def apply_partition_from_grammar(table,partition,grammar):
+    '''
+    This applies a partition if there is a grammar rule that allows us to generate
+    a new lhs.  After unpacking partition, we get a new lhs from the table, using the 
+    grammar:
+
+              (get_lhs,i1Begin,
+                       i2Begin,
+                       i1End,
+                       i2End,
+                       _,
+                       grammar),
+
+    If we are unsuccessful, then this will be False.  In the following swtich dict:
+
+                  {_:(dct_merge_vals,table,
+                                     {i1Begin:{i2End:_}}),
+                   'else':table},
+
+    The _: term means that the result of get_lhs is not False.  If it is, we return
+    the table as-is.  If it is not, then we build a dictionary around the returned 
+    value - {i1Begin:{i2End:_} - and then merge it into the table.
+    '''
+    i1Begin=partition['i1Begin']
+    i1End=partition['i1End']
+    i2Begin=partition['i2Begin']
+    i2End=partition['i2End']
+    
+    return p( table,
+              (get_lhs,i1Begin,
+                       i2Begin,
+                       i1End,
+                       i2End,
+                       _,
+                       grammar),
+                  {_:(dct_merge_vals,table,
+                                     {i1Begin:{i2End:_}}),
+                   'else':table},
+                )
+
+
+@optimize
 def apply_partitions(seq,grammar):
     '''
     This is the main function to run CYK.  Notice that we create a closure function
@@ -296,100 +358,90 @@ def apply_partitions(seq,grammar):
     reduce, and a reduce takes two arguments only.  So we bind the grammar variable
     inside the closure.
 
-    Let's go through apply_partition first.  
+    First, we want to use a closure to wrap apply_partion_from_grammar:
 
-    begin1=partition['begin1'] ...
+    def apply_partition(tb,prt):
 
-    This is a perfect example of how to use the function body to make your code
-    more presentable.
-  
-    (get_lhs,begin1,begin2,end1,end2,_,grammar),
+        return apply_partition_from_grammar(tb,prt,grammar)
 
-    We get the lhs for the partition.
-
-    {_:(dct_merge_vals,table,
-                       {begin1:{end2:_}}),
-     'else':table},
-
-    This is a switch dict:
-
-    {_:(dct_merge_vals,table,
-                       {begin1:{end2:_}}),
-
-    The _ key tests whether the result of the last expression is True.  If it is,
-    we run dct_merge_vals on the table and a new JSON of form 
-
-    {begin1:{end2:lhs}}
-
-    Because dct_merge_vals adds values to the embedded dictionaries, this allows the
-    table to contain a new lhs JSON, indexed by begin1 and end2.
-
-    'else':table},
-
-    Otherwise, return the table as-is.
+    We do this because the reduce only accepts functions with two arguments, the
+    first for the accumulator, and the second for the next item.  Therefore,
+    we want a function, apply_partition, where grammar is bound to grammar
+    in the scope of the apply_partitions function.
 
     Now, we go to the main returned value:
 
-    [(apply_partition,),init_table,partitions],
+              [(apply_partition,),init_table,partitions],
 
-    This is a reduce of the closure function we created.  init_table is the starting
-    value, applied to seq.  partitions is also called on seq, and apply_partition
-    performs the reduce over these partitions.
+    First, this is the syntax for a reduce.  Let's go through it ...
 
-    Basically, the reduce goes over each partition, sees if there is an lhs that can
-    be added to the table, and adds it to the table if it can.  Otherwise, it just
-    returns the table, and the same thing happens with the next partition.
+    init_table returns the starting value of the table from seq:
+
+    {0: {0: {'lhs': 'Det', 'tree': ['Det']}},
+     1: {1: {'lhs': 'N', 'tree': ['N']}},
+     2: {2: {'lhs': 'V', 'tree': ['V']}},
+     3: {3: {'lhs': 'Det', 'tree': ['Det']}},
+     4: {4: {'lhs': 'N', 'tree': ['N']}}}
+
+    partitions generates the partition JSON's which the reduce will iterate through.
+
+    apply_partition is the function that takes a table at a given state, takes a
+    partition, and tries to update that table, depending on whether there is a 
+    grammar rule that can generate a new lhs from that partition.  
 
     It is important to note that, because pype dict manipulations do not guarantee
     imutability, the reduce is not generating a new copy of the table with each 
     partition, but rather an altered version of the original table.
     '''
-    def apply_partition(table,partition):
+    def apply_partition(tb,prt):
 
-        begin1=partition['begin1']
-        end1=partition['end1']
-        begin2=partition['begin2']
-        end2=partition['end2']
-
-        return p( table,
-                  (get_lhs,begin1,begin2,end1,end2,_,grammar),
-                  {_:(dct_merge_vals,table,
-                                     {begin1:{end2:_}}),
-                   'else':table},
-                )
+        return apply_partition_from_grammar(tb,prt,grammar)
 
     return p( seq,
               [(apply_partition,),init_table,partitions],
             )
 
 
+@optimize
 def parse(seq,grammar):
     '''
     This is the main function.
 
-    (get_or_false,(apply_partitions,_,grammar),
-                  0,
-                  lenf - 1),
+    Let's walk through the returned value:
 
-    First, we run apply_partitions on seq.  Then, we check the resulting table to
-    see if there is an element in the table indexed by 0 and the last index of the 
-    string.  If there is, this expression returns that element, otherwise it returns
-    false.
+              (get_or_false,(apply_partitions,_,grammar),
+                            0,
+                            len - 1),
 
-    {_:_['tree'],
-     'else':'No Valid Parse'}
+    First, we run apply_partitions on seq, as described above.  Apply partitions will
+    return a table, as described above.  We want to know if there is a sequence
+    of rules that can generate a parse for the entire string.  If there is, then
+    for a string of five symbols, the table will contain an element at 0 and 4, 
+    meaning there is a tree spanning elements 0 to 4.  If this is true, then the 
+    string is grammatical.  
 
-    Remember, _ as a key in a switch dict is evaluated for its truth value.  So if 
-    the get_or_false returns False, we know the parse is unsuccessful, and we return
-    'No Valid Parse'.  Otherwise, we return the tree of the parse, which will be
+    get_or_false looks at the element of the table starting at 0 and ending at 
+    the length of the sequence - 1.  The starting element is shown by the 0, and the
+    finishing element is defined by 'len - 1', or "the length of seq minus 1".  
+
+    Note that, because the function is optimized, we do not use the PypeVal 
+    lenf instead, the optimizer makes the the necessary changes.  
+
+    If there is no element in this table, the statement will return False.
+
+              {_:_.tree,
+               'else':'No Valid Parse'}
+
+    The {_: evaluates if the get_or_false is False.  If it is False, then we return
+    a string saying 'No Valid Parse'.  Otherwise, we return the tree, which will be
 
     ['S', ['NP', ['Det'], ['N']], ['VP', ['V'], ['NP', ['Det'], ['N']]]]
     '''
     return p( seq,
               (get_or_false,(apply_partitions,_,grammar),
                             0,
-                            lenf - 1),
-              {_:_['tree'],
+                            len - 1),
+              {_:_.tree,
                'else':'No Valid Parse'}
             )
 
